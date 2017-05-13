e6329abe9aa6aec5a38c862d86ad86fb3e5b93dc
alvinlovelife <alvinlovelife@163.com>
2017-05-13 21:35:32 +0800

    228

--- src/com/alvin/algorithm/LeetCode.java
+++ src/com/alvin/algorithm/LeetCode.java
@@ -21,6 +21,30 @@ public class LeetCode {
     }
 
     /**
+     * LeetCode(Array):
+     * 228. Summary Ranges
+     */
+    public static List<String> summaryRanges(int[] nums) {
+        List<String> list=new ArrayList();
+        if(nums.length==1){
+            list.add(nums[0]+"");
+            return list;
+        }
+        for(int i=0;i<nums.length;i++){
+            int a=nums[i];
+            while(i+1<nums.length&&(nums[i+1]-nums[i])==1){
+                i++;
+            }
+            if(a!=nums[i]){
+                list.add(a+"->"+nums[i]);
+            }else{
+                list.add(a+"");
+            }
+        }
+        return list;
+    }
+    
+    /**
      *  LeetCode(Array):
      *  238. Product of Array Except Self
      *  
